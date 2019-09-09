b02e53ac7712f76861f3ed69ac179f623afda733
Anthony Sanchez <ant9821@gmail.com>
2019-09-09 11:32:45 -0500

    update for discord.py 1.0

--- shibeshire/commands/Commands.py
+++ shibeshire/commands/Commands.py
@@ -30,26 +30,24 @@ class Commands(commands.Cog):
     #         await self.bot.delete_message(ctx.message)
 
     @commands.command()
-    @asyncio.coroutine
-    def bills(self, ctx, start_date=None):
+    async def bills(self, ctx, start_date=None):
         """Lists bills due within two weeks of the entered date."""
         if start_date is None:
-            yield from ctx.send("I need to know the date for the bills you want (ex: 10-12-2018).")
+            await ctx.send("I need to know the date for the bills you want (ex: 10-12-2018).")
 
         dates = bills.get_dates(start_date)
         start_date = dates[0]
         end_date = dates[1]
         print('Getting bills beginning from ' + start_date + ' to ' + end_date)
         message = bills.bills(start_date, end_date)
-        yield from ctx.send(message)
+        await ctx.send(message)
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def wordlist(self, ctx, num_of_words=None):
+    async def wordlist(self, ctx, num_of_words=None):
         word_file = 'models/eff_short_wordlist_2_0.txt'
         """Generates a list of words using the diceware method"""
         if num_of_words is None:
-            yield from ctx.send('How many words would you like to generate?')
+            await ctx.send('How many words would you like to generate?')
         else:
             num_of_words = int(num_of_words)
             word_id_length = 4
@@ -59,7 +57,7 @@ class Commands(commands.Cog):
             number = random.randint(0, 100)
             words.generate_wordlist(num_of_words)
             wordlist = words.get_word_list()
-            yield from ctx.send('{}, your {} words are "{}" and your random number is {}'
+            await ctx.send('{}, your {} words are "{}" and your random number is {}'
                                .format(str(ctx.message.author), num_of_words, wordlist, number))
 
 
