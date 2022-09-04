3d60dc1fabed81c1a7b54603235d20cdf594eb6f
Anthony Sanchez <anthony@xebix.com>
2022-09-04 14:35:37 -0500

    remove loader command

--- shibeshire/bot.py
+++ shibeshire/bot.py
@@ -8,7 +8,6 @@ bot = commands.Bot(description="This is my hello world bot", command_prefix="/")
 @bot.event
 async def on_ready():
     bot.load_extension("commands.Commands")
-    bot.load_extension("commands.Loader")
 
 print('Initializing bot ' + str(datetime.datetime.now()))
 bot.run(config.bot_token)
